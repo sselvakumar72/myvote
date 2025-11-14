1d7103acc6b6000cbb593b38085a3c23ab50142d
Selva Kumar <50691844+sselvakumar72@users.noreply.github.com>
2025-11-14 13:39:26 +0530

    Update NullToEmptyObjectSerializer.java

--- myvote-ms/src/main/java/com/lvt/apps/myvote/ms/utils/NullToEmptyObjectSerializer.java
+++ myvote-ms/src/main/java/com/lvt/apps/myvote/ms/utils/NullToEmptyObjectSerializer.java
@@ -1,4 +1,33 @@
 package com.lvt.apps.myvote.ms.utils;
 
-public class NullToEmptyObjectSerializer {
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+
+import java.io.IOException;
+
+public class NullToEmptyObjectSerializer extends JsonSerializer<Object> {
+
+    /**
+     * Method that can be called to ask implementation to serialize
+     * values of type this serializer handles.
+     *
+     * @param value       Value to serialize; can <b>not</b> be null.
+     * @param gen         Generator used to output resulting Json content
+     * @param serializers Provider that can be used to get serializers for
+     *                    serializing Objects value contains, if any.
+     */
+    @Override
+    public void serialize(Object value,
+                          JsonGenerator gen,
+                          SerializerProvider serializers) throws IOException {
+
+        if (value == null) {
+            gen.writeStartObject();
+            gen.writeEndObject();
+        } else {
+            gen.writeObject(value);
+        }
+
+    }
 }
