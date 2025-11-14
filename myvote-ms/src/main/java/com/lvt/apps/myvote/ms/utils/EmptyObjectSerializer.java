5ae59f30e9ee88be5b3a7e29bdfc1135ebc44fcc
Selva Kumar <50691844+sselvakumar72@users.noreply.github.com>
2025-11-14 13:38:54 +0530

    Update EmptyObjectSerializer.java

--- myvote-ms/src/main/java/com/lvt/apps/myvote/ms/utils/EmptyObjectSerializer.java
+++ myvote-ms/src/main/java/com/lvt/apps/myvote/ms/utils/EmptyObjectSerializer.java
@@ -1,4 +1,35 @@
 package com.lvt.apps.myvote.ms.utils;
 
-public class EmptyObjectSerializer {
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.ser.std.StdSerializer;
+import com.optum.ofsc.hba.commonssdk.model.api.util.EmptyObject;
+
+import java.io.IOException;
+import java.io.Serial;
+
+/**
+ * Serializer used in conjunction with DefaultDataDTO to output
+ * empty curly braces in the json output if an object is null
+ */
+public class EmptyObjectSerializer extends StdSerializer<EmptyObject> {
+
+    @Serial
+    private static final long serialVersionUID = 33L;
+
+    public EmptyObjectSerializer() {
+        this(null);
+    }
+
+    protected EmptyObjectSerializer(Class<EmptyObject> t) {
+        super(t);
+    }
+
+    @Override
+    public void serialize(EmptyObject defaultDataDTO, JsonGenerator jsonGenerator,
+                          SerializerProvider serializerProvider) throws IOException {
+
+        jsonGenerator.writeStartObject();
+        jsonGenerator.writeEndObject();
+    }
 }
