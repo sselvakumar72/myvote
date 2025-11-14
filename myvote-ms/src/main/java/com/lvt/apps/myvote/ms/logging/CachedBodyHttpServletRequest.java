47ff91b4759e30a1e8cda2708f94374058c6381f
Selva Kumar <50691844+sselvakumar72@users.noreply.github.com>
2025-11-14 12:08:40 +0530

    Update CachedBodyHttpServletRequest.java

--- myvote-ms/src/main/java/com/lvt/apps/myvote/ms/logging/CachedBodyHttpServletRequest.java
+++ myvote-ms/src/main/java/com/lvt/apps/myvote/ms/logging/CachedBodyHttpServletRequest.java
@@ -1,4 +1,36 @@
 package com.lvt.apps.myvote.ms.logging;
 
-public class CachedBodyHttpServletRequest {
+import jakarta.servlet.ServletInputStream;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletRequestWrapper;
+import org.springframework.util.StreamUtils;
+
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * This class is a wrapper for the HttpServletRequest that caches the body of the request.
+ * This is useful to read the body of the request multiple times. For example, it allows us to log the request body before
+ * it is consumed by the controller.
+ */
+public class CachedBodyHttpServletRequest extends HttpServletRequestWrapper {
+    private final byte[] cachedBody;
+
+    public CachedBodyHttpServletRequest(HttpServletRequest request) throws IOException {
+        super(request);
+        this.cachedBody = StreamUtils.copyToByteArray(request.getInputStream());
+    }
+
+    @Override
+    public ServletInputStream getInputStream() {
+        return new CachedBodyServletInputStream(this.cachedBody);
+    }
+
+    @Override
+    public BufferedReader getReader() {
+        final ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(this.cachedBody);
+        return new BufferedReader(new InputStreamReader(byteArrayInputStream));
+    }
 }
