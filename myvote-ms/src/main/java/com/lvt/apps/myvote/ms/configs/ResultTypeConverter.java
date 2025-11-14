de20ca71632c14161ac6b98c7c6866010f7d239a
Selva Kumar <50691844+sselvakumar72@users.noreply.github.com>
2025-11-14 12:03:54 +0530

    Update ResultTypeConverter.java

--- myvote-ms/src/main/java/com/lvt/apps/myvote/ms/configs/ResultTypeConverter.java
+++ myvote-ms/src/main/java/com/lvt/apps/myvote/ms/configs/ResultTypeConverter.java
@@ -1,4 +1,15 @@
 package com.lvt.apps.myvote.ms.configs;
 
-public class ResultTypeConverter {
+import com.optum.ofsc.bds.accounts.model.ResultType;
+import lombok.NonNull;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ResultTypeConverter implements Converter<String, ResultType> {
+    @Override
+    public ResultType convert(@NonNull String source) {
+        return ResultType.fromValue(source);
+    }
 }
+
