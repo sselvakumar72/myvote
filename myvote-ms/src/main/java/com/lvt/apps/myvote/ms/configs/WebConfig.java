ad1023d61b07afc2fa661cb55ac6ed0684ec6202
Selva Kumar <50691844+sselvakumar72@users.noreply.github.com>
2025-11-14 12:05:06 +0530

    Update WebConfig.java

--- myvote-ms/src/main/java/com/lvt/apps/myvote/ms/configs/WebConfig.java
+++ myvote-ms/src/main/java/com/lvt/apps/myvote/ms/configs/WebConfig.java
@@ -1,4 +1,19 @@
 package com.lvt.apps.myvote.ms.configs;
 
-public class WebConfig {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    private final RequestValidationInterceptor requestValidationInterceptor;
+
+    public WebConfig(RequestValidationInterceptor requestValidationInterceptor) {
+        this.requestValidationInterceptor = requestValidationInterceptor;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(requestValidationInterceptor);
+    }
 }
